356faee0fb8dbeb5192001f72b5e64c44dbbd975
DavidUKr <davidkrinurs@gmail.com>
2024-10-23 11:17:51 +0300

    first commit

---
+++ src/main/java/org/app/battleshiproyale/BattleshipRoyaleApplication.java
@@ -0,0 +1,13 @@
+package org.app.battleshiproyale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipRoyaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattleshipRoyaleApplication.class, args);
+    }
+
+}
