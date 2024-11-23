020ea2f2a40f4af21e55dd92679fa1e6d63aebe2
DavidUKr <davidkrinurs@gmail.com>
2024-11-23 23:09:22 +0200

    fixed server to run

--- src/main/java/org/app/battleshiproyale/control_multi/GameController.java
+++ src/main/java/org/app/battleshiproyale/control_multi/GameController.java
@@ -1,35 +1,35 @@
-package org.app.battleshiproyale.control_multi;
-
-import lombok.RequiredArgsConstructor;
-import org.app.battleshiproyale.model.GameStateDTO;
-import org.app.battleshiproyale.model.HitResultDTO;
-import org.app.battleshiproyale.service.GameService;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping(value = "BattleshipRoyale/api/v2/games")
-@RequiredArgsConstructor
-public class GameController {
+//package org.app.battleshiproyale.control_multi;
 //
-//    private final GameService gameService;
+//import lombok.RequiredArgsConstructor;
+//import org.app.battleshiproyale.model.GameStateDTO;
+//import org.app.battleshiproyale.model.HitResultDTO;
+//import org.app.battleshiproyale.service.GameService;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
 //
-//    @PostMapping(value = "/generate/{session_id}", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<GameStateDTO> generateGame(@PathVariable String session_id) {
-//        GameStateDTO gameState = gameService.generateGame(session_id);
-//        return ResponseEntity.ok(gameState);
-//    }
-//
-//    @PostMapping(value = "/hit/{session_id}/{player_id}", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<HitResultDTO> hit(@PathVariable String session_id, @PathVariable String player_id) {
-//        HitResultDTO hitResult = gameService.hit(session_id, player_id);
-//        return ResponseEntity.ok(hitResult);
-//    }
-//
-//    @GetMapping(value = "/state/{session_id}", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<GameStateDTO> getGameState(@PathVariable String session_id) {
-//        GameStateDTO gameState = gameService.getGameState(session_id);
-//        return ResponseEntity.ok(gameState);
-//    }
-}
+//@RestController
+//@RequestMapping(value = "BattleshipRoyale/api/v2/games")
+//@RequiredArgsConstructor
+//public class GameController {
+////
+////    private final GameService gameService;
+////
+////    @PostMapping(value = "/generate/{session_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+////    public ResponseEntity<GameStateDTO> generateGame(@PathVariable String session_id) {
+////        GameStateDTO gameState = gameService.generateGame(session_id);
+////        return ResponseEntity.ok(gameState);
+////    }
+////
+////    @PostMapping(value = "/hit/{session_id}/{player_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+////    public ResponseEntity<HitResultDTO> hit(@PathVariable String session_id, @PathVariable String player_id) {
+////        HitResultDTO hitResult = gameService.hit(session_id, player_id);
+////        return ResponseEntity.ok(hitResult);
+////    }
+////
+////    @GetMapping(value = "/state/{session_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+////    public ResponseEntity<GameStateDTO> getGameState(@PathVariable String session_id) {
+////        GameStateDTO gameState = gameService.getGameState(session_id);
+////        return ResponseEntity.ok(gameState);
+////    }
+//}
--- src/main/java/org/app/battleshiproyale/control_multi/GlobalController.java
+++ src/main/java/org/app/battleshiproyale/control_multi/GlobalController.java
@@ -1,23 +1,23 @@
-package org.app.battleshiproyale.control_multi;
-
-import lombok.RequiredArgsConstructor;
-import org.app.battleshiproyale.model.StatsDTO;
-import org.app.battleshiproyale.service.GlobalService;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "BattleshipRoyale/api/v2/global")
-@RequiredArgsConstructor
-public class GlobalController {
-
-//    private final GlobalService globalService;
+//package org.app.battleshiproyale.control_multi;
 //
-//    @GetMapping(value="", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public StatsDTO getGlobalStats(){
-//        return globalService.getGlobalStats();
-//    }
-
-}
+//import lombok.RequiredArgsConstructor;
+//import org.app.battleshiproyale.model.StatsDTO;
+//import org.app.battleshiproyale.service.GlobalService;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping(value = "BattleshipRoyale/api/v2/global")
+//@RequiredArgsConstructor
+//public class GlobalController {
+//
+////    private final GlobalService globalService;
+////
+////    @GetMapping(value="", produces = MediaType.APPLICATION_JSON_VALUE)
+////    public StatsDTO getGlobalStats(){
+////        return globalService.getGlobalStats();
+////    }
+//
+//}
