3bc95f28e112660602981095b5707323a0539c2a
DavidUKr <davidkrinurs@gmail.com>
2024-12-07 23:11:49 +0200

    implemented player constructor

--- src/main/java/org/app/battleshiproyale/model/Player.java
+++ src/main/java/org/app/battleshiproyale/model/Player.java
@@ -3,6 +3,7 @@ package org.app.battleshiproyale.model;
 import lombok.Data;
 import org.app.battleshiproyale.game.game_elements.ships.BaseShip;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -11,8 +12,11 @@ public class Player {
     public int stamina;
     private List<BaseShip> ships;
     private boolean isReadyForBattle=false;
+
     public Player(String id) {
         this.id = id;
+        this.stamina = 0;
+        this.ships = new ArrayList<>();
     }
 }
 
