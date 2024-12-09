a79c1883b1093f7e11970d34471d68148639ef77
DavidUKr <davidkrinurs@gmail.com>
2024-12-09 17:43:29 +0200

    fixed merge bugs

--- src/main/java/org/app/battleshiproyale/game/game_elements/Player.java
+++ src/main/java/org/app/battleshiproyale/game/game_elements/Player.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
-public class Player implements Runnable {
+public class Player {
     @Getter
     private final int playerId;
     private final Random random = new Random(); // For generating random moves
@@ -16,45 +16,6 @@ public class Player implements Runnable {
     public Player(int playerId, BattleGrid battleGrid) {
         this.playerId = playerId;
         this.battleGrid = battleGrid;
-    }
-
-    @Override
-    public void run() {
-
-        GridCell[][] playerGrid = (playerId == 0) ? battleGrid.getPlayer1Grid() : battleGrid.getPlayer2Grid();
-        int gridWidth = playerGrid.length;
-        int gridHeight = playerGrid[0].length;
-
-        while (visitedCells.size() < gridWidth * gridHeight && !battleGrid.isFinished()) {
-            int x = random.nextInt(gridWidth);
-            int y = random.nextInt(gridHeight);
-            String cellKey = x + "," + y;
-
-            if (visitedCells.contains(cellKey)) {
-                continue;
-            } else {
-                visitedCells.add(cellKey);
-            }
-
-            try {
-                battleGrid.gridLock.lock();
-                boolean hitResult = battleGrid.hit(x, y, playerId, playerGrid, gridWidth, gridHeight);
-                if (hitResult) {
-                    System.out.println("Player " + (playerId + 1) + " successfully hit at (" + x + ", " + y + ")");
-                } else {
-                    System.out.println("Player " + (playerId + 1) + " missed at (" + x + ", " + y + ")");
-                }
-            } finally {
-                battleGrid.gridLock.unlock();
-            }
-
-            try {
-                Thread.sleep(250);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                e.printStackTrace();
-            }
-        }
     }
 }
 
