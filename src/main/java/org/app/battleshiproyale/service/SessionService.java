15d9f21a6ed13e51cb34d2b64fe3acdb8a55426c
Ovidiu Raul <calinovidiu15@yahoo.ro>
2024-12-09 17:39:04 +0300

    commit

--- src/main/java/org/app/battleshiproyale/service/SessionService.java
+++ src/main/java/org/app/battleshiproyale/service/SessionService.java
@@ -2,11 +2,13 @@ package org.app.battleshiproyale.service;
 
 import lombok.RequiredArgsConstructor;
 import org.app.battleshiproyale.game.Game;
+import org.app.battleshiproyale.model.Player;
 import org.app.battleshiproyale.model.PlayerMap;
 import org.app.battleshiproyale.model.PlayerMapDTO;
 import org.app.battleshiproyale.utils.MapUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -44,7 +46,7 @@ public class SessionService {
         return false;
     }
 
-    public List<String> getPlayersReadyStatus() {
+    public ArrayList<Player> getPlayersReadyStatus() {
         return game.getReadyPlayersIds();
     }
 
