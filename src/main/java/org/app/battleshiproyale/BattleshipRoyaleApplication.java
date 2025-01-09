db291403a0e622bd6bf5dc5dbb438ec43b4f3f35
AD-Costut <costut_ad05@yahoo.com>
2025-01-09 22:55:42 +0200

    Try to solve CORS error

--- src/main/java/org/app/battleshiproyale/BattleshipRoyaleApplication.java
+++ src/main/java/org/app/battleshiproyale/BattleshipRoyaleApplication.java
@@ -2,6 +2,10 @@ package org.app.battleshiproyale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class BattleshipRoyaleApplication {
@@ -10,5 +14,19 @@ public class BattleshipRoyaleApplication {
         SpringApplication.run(BattleshipRoyaleApplication.class, args);
     }
 
+    @Bean
+    public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("https://battle-ship-royale.web.app");
+    config.addAllowedOrigin("http://localhost:3000"); 
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+}
 
 }
