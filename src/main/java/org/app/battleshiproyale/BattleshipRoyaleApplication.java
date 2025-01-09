c45741773021bd1f46c0af018d92db7d50eba926
AD-Costut <costut_ad05@yahoo.com>
2025-01-09 23:45:20 +0200

    CORS

--- src/main/java/org/app/battleshiproyale/BattleshipRoyaleApplication.java
+++ src/main/java/org/app/battleshiproyale/BattleshipRoyaleApplication.java
@@ -9,11 +9,6 @@ import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class BattleshipRoyaleApplication {
@@ -22,19 +17,17 @@ public class BattleshipRoyaleApplication {
         SpringApplication.run(BattleshipRoyaleApplication.class, args);
     }
 
-    @Configuration
-    public class DynamicCorsConfig {
-        @Bean
-        public CorsFilter corsFilter() {
-            CorsConfiguration config = new CorsConfiguration();
-            config.setAllowCredentials(true);
-            config.addAllowedOrigin("https://battle-ship-royale.web.app");
-            config.addAllowedMethod("*");
-            config.addAllowedHeader("*");
-    
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            source.registerCorsConfiguration("/**", config);
-            return new CorsFilter(source);
-        }
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); 
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://battle-ship-royale.web.app");
+        config.addAllowedOrigin("https://battle-ship-royale.web.app/"); 
+        config.addAllowedHeader("*"); 
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config); 
+        return new CorsFilter(source);
     }
 }
